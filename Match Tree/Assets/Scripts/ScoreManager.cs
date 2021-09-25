73ea0c4c451b82ad0ae89f44286e0db02a0cf0f9
MDrFaza <mdrfaza21@gmail.com>
2021-09-25 22:55:55 +0700

    Basic Game Done

---
+++ Match Tree/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static int highScore;    
+    public Text highScoreText;
+    public Text currentScoreText;
+
+    private void Update()
+    {
+        highScoreText.text = ScoreManager.Instance.HighScore.ToString();
+        currentScoreText.text = ScoreManager.Instance.CurrentScore.ToString();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    #region Singleton
+
+    private static ScoreManager _instance = null;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<ScoreManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: ScoreManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public int tileRatio;
+    public int comboRatio;
+
+    public int HighScore { get { return highScore; } }
+    public int CurrentScore { get { return currentScore; } }
+
+    private int currentScore;
+
+    private void Start()
+    {
+        ResetCurrentScore();
+    }
+
+    public void ResetCurrentScore()
+    {
+        currentScore = 0;
+    }
+
+    public void IncrementCurrentScore(int tileCount, int comboCount)
+    {
+        currentScore += (tileCount * tileRatio) * (comboCount * comboRatio);
+        SoundManager.Instance.PlayScore(comboCount > 1);
+    }
+
+    public void SetHighScore()
+    {
+        if(currentScore > highScore){
+            highScore = currentScore;
+        }
+    }
+}
---
+++ Match Tree/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text timeText;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private string GetTimeString(float timeRemaining)
+    {
+        int minute = Mathf.FloorToInt(timeRemaining / 60);
+        int second = Mathf.FloorToInt(timeRemaining % 60);
+
+        return string.Format("{0} : {1}", minute.ToString(), second.ToString());
+    }
+
+    #region Singleton
+
+    private static TimeManager _instance = null;
+
+    public static TimeManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<TimeManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: TimeManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public int duration;
+
+    private float time;
+
+    private void Start()
+    {
+        time = 0;
+    }
+
+    private void Update()
+    {
+        if (GameFlowManager.Instance.IsGameOver)
+        {
+            return;
+        }
+
+        if (time > duration)
+        {
+            GameFlowManager.Instance.GameOver();
+            return;
+        }
+
+        time += Time.deltaTime;
+        timeText.text = GetTimeString(TimeManager.Instance.GetRemainingTime() + 1);
+    }
+
+    public float GetRemainingTime()
+    {
+        return duration - time;
+    }
+}
